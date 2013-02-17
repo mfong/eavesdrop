ff140d39348c9e5891262e2c5bacf4083a28833d
Adib Behjat <abehjat@gmail.com>
2013-02-16 19:24:24 -0800

    Expanded Twitter Function
    Trial

--- app/what.php
+++ app/what.php
@@ -8,28 +8,44 @@ $access_secret = "rwPBADZWflr9zJNQWB1L4F6ANOFX1OaQBI4FIxlX6qY";
   
 $connection = new TwitterOAuth ($consumer_key ,$consumer_secret , $access_key , $access_secret );
 
-/*$parameters['screen_name'] = 'diplo';
-//$parameters['count'] = 2;
+$search_tweets = 'https://api.twitter.com/1.1/search/tweets.json';
+$search_users  = 'https://api.twitter.com/1.1/users/search.json';
+$read_timeline = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 
-$result = $connection->get('statuses/user_timeline', $parameters);*/
 
-//$p['q'] = 'soundcloud.com from:diplo filter:links';
-//$p['q'] = 'snd.sc from:' . $_GET['q'] . ' filter:links';
 $p['q'] = 'soundcloud from:' . $_GET['q'] . ' filter:links';
-$p['count'] = 5;
-$result = $connection->get('https://api.twitter.com/1.1/search/tweets.json', $p);
+$result = $connection->get($search_tweets, $p);
+
+//Define Variables
+$url_depo = array();
+$mentions = array();
 
 foreach($result->statuses as $s) {
 	foreach($s->entities->urls as $url) {
 		//if ((stristr($url->expanded_url, 'soundcloud.com') !== FALSE) || (stristr($url->expanded_url, 'snd.sc') !== FALSE)) {
-			echo json_encode($url->expanded_url);
-			return false;
+			$value = $url->expanded_url;
+			array_push($url_depo, $value);
 		//}
 	}
 }
 
-//print_r($result);
+//Clear Data sets
+unset($p);
+unset($result);
+unset($value);
 
+$p['screen_name'] = $_GET['q'];
+
+$result = $connection->get($read_timeline, $p);
+
+foreach($result as $twitter_post)
+{
+	foreach($twitter_post->entities->user_mentions as $shout_out)
+	{
+		$value = $shout_out->screen_name;
+		array_push($mentions, $value);
+	}
+}
 
 //$connection->post('statuses/update', array('status' => $message));
 
