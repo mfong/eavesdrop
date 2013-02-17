70f397d8e17847d199c722a92affcdc46cb85148
Adib Behjat <abehjat@gmail.com>
2013-02-16 19:59:18 -0800

    Clean Functions
    Clean functions of dataset

--- app/what.php
+++ app/what.php
@@ -12,14 +12,12 @@ $search_tweets = 'https://api.twitter.com/1.1/search/tweets.json';
 $search_users  = 'https://api.twitter.com/1.1/users/search.json';
 $read_timeline = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 
-
-$p['q'] = 'soundcloud from:' . $_GET['q'] . ' filter:links';
-$result = $connection->get($search_tweets, $p);
-
 //Define Variables
 $url_depo = array();
 $mentions = array();
+$get_query = $_GET['q'];
 
+<<<<<<< HEAD
 foreach($result->statuses as $s) {
 	foreach($s->entities->urls as $url) {
 		$urlLocation = get_location_header($url->expanded_url);
@@ -29,27 +27,100 @@ foreach($result->statuses as $s) {
 		}
 	}
 }
-
-//Clear Data sets
-unset($p);
-unset($result);
-unset($value);
-
-$p['screen_name'] = $_GET['q'];
-
-$result = $connection->get($read_timeline, $p);
-
-foreach($result as $twitter_post)
+=======
+//Captures the latest Tweet based on the conditions
+function search_tweets ($search_tweets, $get_query, $connection)
 {
-	foreach($twitter_post->entities->user_mentions as $shout_out)
+	//Define Variables
+	$array = array();
+>>>>>>> Clean Functions
+
+	//Capture URL
+	$p['q'] = 'soundcloud from:' . $get_query . ' filter:links';
+	$p['count'] = 5;
+	$result = $connection->get($search_tweets, $p);
+
+	foreach($result->statuses as $s) 
 	{
+		foreach($s->entities->urls as $url) 
+		{
+			$urlLocation = get_location_header($url->expanded_url);
+			if ((stristr($urlLocation, 'soundcloud.com') !== FALSE) || (stristr($urlLocation, 'snd.sc') !== FALSE)) 
+			{
+				$value = $url->expanded_url;
+				array_push($array, $value);
+			} // End of IF
+		} //End of Foreach
+	} // End of Foreach
+
+	return $array;
+
+}//End of function
+
+//Read the user's timeline
+function read_timeline($read_timeline, $get_query, $connection)
+{
+	//Define Variables
+	$array = array();
+
+	$p['screen_name'] = $get_query;
+
+	$result = $connection->get($read_timeline, $p);
+
+	foreach($result as $twitter_post)
+	{
+<<<<<<< HEAD
 		$value = $shout_out->screen_name;
 		if (!in_array($value, $mentions)) {
 			array_push($mentions, $value);
 		}
+=======
+		foreach($twitter_post->entities->user_mentions as $shout_out)
+		{
+			$value = $shout_out->screen_name;
+				if (!in_array($value, $array)) 
+				{
+					array_push($array, $value);
+				} //End of IF Condition
+
+		} //End of foreach
+
+	} //End of forreach
+
+	return $array;
+
+}//End of function
+
+// this will return the long url from (most) url shorteners
+function get_location_header($url) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'MHD 2013');
+	curl_setopt($ch, CURLOPT_REFERER, 'http://tbd.com');
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_NOBODY, true);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	if ($response) {
+	   preg_match_all('#Location:\s?([^\s]+)#i', $response, $match);
+	    
+	    if (isset($match[1])) {
+	        return end($match[1]);
+	       }
+>>>>>>> Clean Functions
 	}
+	
+	return false;
 }
 
+<<<<<<< HEAD
 $return = new ArrayObject();
 $return['urls'] = $url_depo;
 $return['mentions'] = $mentions;
@@ -57,6 +128,17 @@ $return['mentions'] = $mentions;
 echo json_encode($return);
 
 //$connection->post('statuses/update', array('status' => $message));
+=======
+//Capture the data from the functions
+$url_depo = search_tweets($search_tweets, $get_query, $connection);
+$mention  = read_timeline($read_timeline, $get_query, $connection);
+
+$return = new ArrayObject();
+$return['urls'] = $url_depo;
+$return['mentions'] = $mention;
+
+echo json_encode($return);
+>>>>>>> Clean Functions
 
 
 
